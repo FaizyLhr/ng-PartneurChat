b825bf74adf3adadc510482ebc0373c6f30744e9
Faizy <FaizyLhr>
2021-12-22 14:55:08 +0500

    route index fixes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,10 +1,10 @@
 let router = require("express").Router();
 
-router.use("/", require("./users"));
-router.use("/", require("./chat"));
-router.use("/", require("./friend"));
-router.use("/", require("./image"));
-router.use("/", require("./audios"));
-router.use("/", require("./videos"));
+router.use("/users", require("./users"));
+router.use("/chats", require("./chat"));
+router.use("/friends", require("./friend"));
+router.use("/images", require("./image"));
+router.use("/audios", require("./audios"));
+router.use("/videos", require("./videos"));
 
 module.exports = router;

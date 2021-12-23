59d6a0fbf602fff0d74baa8fd92fad2d1c990b10
Faizy <FaizyLhr>
2021-12-23 15:57:02 +0500

    freind api Done Initially

--- server/routes/api/friend.js
+++ server/routes/api/friend.js
@@ -4,22 +4,24 @@ let {
 	OkResponse,
 	BadRequestResponse,
 	UnauthorizedResponse,
+	ForbiddenResponse
 } = require("express-http-response");
-
-const { isToken, isAuthentic } = require("../auth");
-// const { query } = require("express-validator");
 
 const FriendModel = require("../../models/Friend");
 const UserModel = require("../../models/User");
 
 // General Check
-router.get("/", function (req, res, next) {
-	return next(new OkResponse({ message: `Friends Api's are working` }));
-});
+// router.get("/", function (req, res, next) {
+// 	return next(new OkResponse({
+// 		message: `Friends Api's are working`
+// 	}));
+// });
 
-// get friend for every time friendSlug given
-router.param("friendSlug", function (req, res, next, slug) {
-	FriendModel.findOne({ slug }).then(function (friend) {
+// get friend for every time Friend ID given
+router.param("friendID", function (req, res, next, _id) {
+	FriendModel.findOne({
+		_id
+	}).then(function (friend) {
 		if (!friend) {
 			return next(new BadRequestResponse("No friend Found"));
 		}
@@ -30,27 +32,48 @@ router.param("friendSlug", function (req, res, next, slug) {
 	});
 });
 
-// get user for every time mail given
-router.param("email", (req, res, next, email) => {
-	UserModel.findOne({ email }, (err, user) => {
+// get Sender for every time Sender ID given
+router.param("senderID", (req, res, next, _id) => {
+	UserModel.findOne({
+		_id
+	}, (err, user) => {
 		if (!err && user !== null) {
 			// console.log(user);
-			req.emailUser = user;
+			req.sender = user;
 			return next();
 		}
-		next(new BadRequestResponse("User not found!", 423));
-		return;
+		return next(new BadRequestResponse("User not found!", 423));;
 	});
 });
 
-// Add a friend Chat
-router.post("/addFriend/:email", isToken, (req, res, next) => {
-	// console.log(req.emailUser);
-	// console.log(req.user);
+// get Receiver for every time Receiver ID given
+router.param("receiverID", (req, res, next, _id) => {
+	// console.log(_id);
+	UserModel.findOne({
+		_id
+	}, (err, user) => {
+		if (!err && user !== null) {
+			// console.log(user);
+			req.receiver = user;
+			return next();
+		}
+		return next(new BadRequestResponse("User not found!", 423));;
+	});
+});
+
+// Add a friend Chat  //Done
+router.post("/add/:receiverID/:senderID", (req, res, next) => {
+	// console.log("SEnder", req.sender);
+	// console.log("Receiver", req.receiver);
 	let query = {
-		$or: [
-			{ user1: req.emailUser._id, user2: req.user._id },
-			{ user2: req.emailUser._id, user1: req.user._id },
+		$or: [{
+				user1: req.sender._id,
+				user2: req.receiver._id
+			},
+			{
+				user2: req.sender._id,
+				user1: req.receiver._id
+			},
 		],
 	};
 	// console.log(query);
@@ -59,14 +82,21 @@ router.post("/addFriend/:email", isToken, (req, res, next) => {
 			// console.log(match);
 			// console.log(match.length !== 0);
 			if (match.length !== 0) {
-				next(new OkResponse({ message: "Already Friends" }));
+				next(new OkResponse({
+					message: "Already Friends"
+				}));
 			} else {
 				// console.log("friend");
-				new FriendModel({ user1: req.user._id, user2: req.emailUser._id })
+				new FriendModel({
+						user1: req.sender._id,
+						user2: req.receiver._id
+					})
 					.save()
 					.then((friend) => {
 						// console.log(friend);
-						next(new OkResponse({ friend: friend }));
+						next(new OkResponse({
+							friend
+						}));
 					})
 					.catch((err) => {
 						next(new BadRequestResponse(err));
@@ -78,42 +108,25 @@ router.post("/addFriend/:email", isToken, (req, res, next) => {
 		});
 });
 
-// View Friend's Messages
-{
-	// router.get("/friend/:friendSlug", isToken, (req, res, next) => {
-	// 	// console.log(req.friend);
-	// 	req.friend
-	// 		.populate("user1", "email username")
-	// 		.populate("user2", "email username")
-	// 		.execPopulate()
-	// 		.then(function (results) {
-	// 			// console.log(results);
-	// 			next(new OkResponse({ friend: results }));
-	// 		})
-	// 		.catch((err) => next(new BadRequestResponse(err)));
-	// });
-}
-
-// View All Friend's
-router.get("/friends", isToken, function (req, res, next) {
+// View All Friend's //Done
+router.get("/:senderID", function (req, res, next) {
 	const options = {
 		page: +req.query.page || 1,
 		limit: +req.query.limit || 10,
-		populate: [
-			{
+		populate: [{
 				path: "user1",
 				model: "User",
-				select: "username",
+				// select: "username",
 			},
 			{
 				path: "user2",
 				model: "User",
-				select: "username",
+				// select: "username",
 			},
 			{
 				path: "lastMessage",
 				model: "Chat",
-				select: "receiver sender message createdAt",
+				// select: "receiver sender message createdAt",
 			},
 		],
 		sort: {
@@ -121,8 +134,14 @@ router.get("/friends", isToken, function (req, res, next) {
 		},
 	};
 	let query = {
-		$or: [{ user1: req.user._id }, { user2: req.user._id }],
+		$or: [{
+			user1: req.sender._id
+		}, {
+			user2: req.sender._id
+		}],
 	};
+	query.isVisited = true;
+	query.status = 1;
 	// console.log(query);
 	FriendModel.paginate(query, options, (err, result) => {
 		if (err) {
@@ -130,16 +149,39 @@ router.get("/friends", isToken, function (req, res, next) {
 			return next(new BadRequestResponse("Server Error"));
 		} else {
 			// console.log(result);
-			return next(new OkResponse({ result: result.docs }));
+			return next(new OkResponse({
+				result: result.docs
+			}));
 		}
 	});
 });
 
-// Delete Friend
-router.delete("/delFriend/:friendSlug", isToken, async (req, res, next) => {
+// Change is Visited Bit to true //Done
+router.put("/visit/:friendID", (req, res, next) => {
+	// console.log("Friend", req.friend);
+	if (req.friend.isVisited === true) {
+		// console.log("IF");
+		return next(new ForbiddenResponse("Friend Already Visited"));
+	}
+
+	req.friend.isVisited = true;
+
+	req.friend.save((err, result) => {
+		if (err) {
+			// console.log(err);
+			return next(new BadRequestResponse(err));;
+		} else {
+			// console.log("result", result);
+			return next(new OkResponse(result));
+		}
+	});
+});
+
+// Delete Friend //Done
+router.delete("/del/:friendID/:senderID", async (req, res, next) => {
 	if (
-		req.user._id.toString() === req.friend.user1.toString() ||
-		req.user._id.toString() === req.friend.user2.toString()
+		req.sender._id.toString() === req.friend.user1.toString() ||
+		req.sender._id.toString() === req.friend.user2.toString()
 	) {
 		req.friend
 			.remove()
@@ -156,11 +198,9 @@ router.delete("/delFriend/:friendSlug", isToken, async (req, res, next) => {
 	}
 });
 
-// Delete Friend Chat
+// Delete Friend Chat //Done
 router.delete(
-	"/delChats/:friendSlug",
-	isToken,
-	isAuthentic,
+	"/delChats/:friendID",
 	(req, res, next) => {
 		req.friend.chatMessages = [];
 		// console.log(req.toll);

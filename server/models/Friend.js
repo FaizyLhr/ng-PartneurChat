5ac736646194766b3f94830a30cf7fbae445ce93
Faizy <FaizyLhr>
2021-12-23 15:56:15 +0500

    Friend Schema Fixes

--- server/models/Friend.js
+++ server/models/Friend.js
@@ -3,70 +3,67 @@ const slug = require("slug");
 const mongoosePaginate = require("mongoose-paginate-v2");
 const uniqueValidator = require("mongoose-unique-validator");
 
-const FriendsSchema = new mongoose.Schema(
-	{
-		slug: { type: String, lowercase: true, unique: true },
-		user1: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-		},
-		user2: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-		},
-		status: {
-			type: Number,
-			default: 1,
-			enum: [
-				1, // 1: Friend
-				2, // 2: Unfriend
-			],
-		},
-		lastMessage: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Chat",
-		},
-		chatMessages: [
-			{
-				type: mongoose.Schema.Types.ObjectId,
-				ref: "Chat",
-			},
-		],
-		favorites: [
-			{
-				type: mongoose.Schema.Types.ObjectId,
-				ref: "Chat",
-			},
-		],
-		images: [
-			{
-				type: mongoose.Schema.Types.ObjectId,
-				ref: "Image",
-			},
-		],
-		audios: [
-			{
-				type: mongoose.Schema.Types.ObjectId,
-				ref: "Audio",
-			},
-		],
-		videos: [
-			{
-				type: mongoose.Schema.Types.ObjectId,
-				ref: "Video",
-			},
-		],
-		currentStatus: {
-			type: Number,
-			default: 2,
-			enum: [
-				1, // 1: online
-				2, // 2: away
-			],
-		},
+const FriendsSchema = new mongoose.Schema({
+	slug: {
+		type: String,
+		lowercase: true,
+		unique: true
 	},
-	{ timestamps: true }
-);
+	user1: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+	},
+	user2: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+	},
+	status: {
+		type: Number,
+		default: 1,
+		enum: [
+			1, // 1: Friend
+			2, // 2: Unfriend
+		],
+	},
+	lastMessage: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Chat",
+	},
+	chatMessages: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Chat",
+	}, ],
+	isVisited: {
+		type: Boolean,
+		default: false
+	},
+	favorites: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Chat",
+	}, ],
+	images: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Image",
+	}, ],
+	audios: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Audio",
+	}, ],
+	videos: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Video",
+	}, ],
+	currentStatus: {
+		type: Number,
+		default: 2,
+		enum: [
+			1, // 1: online
+			2, // 2: away
+		],
+	},
+}, {
+	timestamps: true
+});
 
 FriendsSchema.plugin(mongoosePaginate);
 // FriendsSchema.plugin(uniqueValidator, { message: "is already taken." });
